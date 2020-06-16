8ad8384389bc3d70fb39fd8816825b6fcb879b7c
Murat Altintas <murat.altintas.34@gmail.com>
2020-06-16 09:15:33 +0300

    v5.2

---
+++ lib/model/firebaseBook.dart
@@ -0,0 +1,37 @@
+// To parse this JSON data, do
+//
+//     final firebaseBook = firebaseBookFromJson(jsonString);
+
+import 'dart:convert';
+
+List<FirebaseBook> firebaseBookFromJson(String str) => List<FirebaseBook>.from(json.decode(str).map((x) => FirebaseBook.fromJson(x)));
+
+String firebaseBookToJson(List<FirebaseBook> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class FirebaseBook {
+  String image;
+  String publisher;
+  String title;
+  String authors;
+
+  FirebaseBook({
+    this.image,
+    this.publisher,
+    this.title,
+    this.authors,
+  });
+
+  factory FirebaseBook.fromJson(Map<String, dynamic> json) => FirebaseBook(
+        image: json["image"],
+        publisher: json["publisher"],
+        title: json["title"],
+        authors: json["authors"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "image": image,
+        "publisher": publisher,
+        "title": title,
+        "authors": authors,
+      };
+}
