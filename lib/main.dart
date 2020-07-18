81174b011222fe7717fd3f4fdd7da87c39bb1689
Murat-Altintas <murat.altintas.34@gmail.com>
2020-07-18 19:18:58 +0300

    ver 7.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grade_point_avarage/CreateAccount.dart';
+import 'package:grade_point_avarage/ResaultPage.dart';
 
 import 'LoginPage.dart';
 import 'style.dart';
@@ -28,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       //backgroundColor: Colors.lightBlueAccent.shade100,
-      body: LoginPage(),
+      body: ResaultPage(),
 //      body: BookshelfPage2(),
     );
   }
